491f2a7162ea47d0b2bd57d22f2b4bbbe99dad26
Jihyun <034179@naver.com>
2022-04-21 23:43:28 +0900

    22.4.21 PS

---
+++ BOJ/python/DP/11053.py
@@ -0,0 +1,9 @@
+N = int(input())
+num = list(map(int, input().split()))
+dp = [1]*N
+
+for i in range(N):    
+    for j in range(0,i):
+        if num[j] < num[i]:
+            dp[i] = max(dp[i], dp[j]+1)
+print(max(dp))
