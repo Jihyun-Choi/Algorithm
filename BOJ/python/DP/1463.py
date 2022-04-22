4bf276f61b0cc580c2bb9ce08bf7837c2369cd9d
Jihyun <034179@naver.com>
2022-04-22 23:07:18 +0900

    22.4.22 PS

---
+++ BOJ/python/DP/1463.py
@@ -0,0 +1,27 @@
+# 1
+N = int(input())
+dp = [0]*(N+1)
+dp[1] = 0
+dp[2] = 1
+for i in range(3, N+1):
+    dp[i] = dp[i-1] + 1
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2]+1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3]+1)
+print(dp[N])
+
+# 2
+n = int(input())
+dp = [0] * (n+1)
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + 1
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2]+1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
+print(dp[n])
+
+""" 1463
+# 1 런타임에러(IndesError)
+"""
