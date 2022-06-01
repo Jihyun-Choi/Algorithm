c9dcbb555fa8401347f4da25e17663e90ba0d392
Jihyun <034179@naver.com>
2022-06-01 23:39:03 +0900

    22.6.1 PS

---
+++ BOJ/python/backtracking/15649.py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+S = []
+def dfs():
+    if len(S) == M:
+        print(' '.join(map(str,S)))
+        return
+    
+    for i in range(1,N+1):
+        if i not in S:
+            S.append(i)
+            dfs()
+            S.pop()
+dfs()
