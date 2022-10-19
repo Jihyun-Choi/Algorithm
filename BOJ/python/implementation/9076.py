d0148866eb70cb82e561fb9f06b848f816835858
Jihyun-Choi <034179@naver.com>
2022-10-19 18:29:12 +0900

    22.10.19 PS

---
+++ BOJ/python/implementation/9076.py
@@ -0,0 +1,9 @@
+T = int(input())
+for i in range(T):
+    score = list(map(int, input().split()))
+    score.remove(max(score))
+    score.remove(min(score))
+    if max(score) - min(score) >= 4:
+        print('KIN')
+    else:
+        print(sum(score))
