55807950a1a0a8ceeb49ff32910f437b37c0ccad
Jihyun-Choi <034179@naver.com>
2022-10-20 23:37:18 +0900

    22.10.20 PS

---
+++ BOJ/python/implementation/1547.py
@@ -0,0 +1,8 @@
+N = int(input())
+cup = [1,2,3]
+for _ in range(N):
+    X, Y = map(int, input().split())
+    x = cup.index(X)
+    y = cup.index(Y)
+    cup[x], cup[y] = cup[y], cup[x]
+print(cup[0])
