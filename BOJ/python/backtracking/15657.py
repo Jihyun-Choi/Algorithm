b06e0a3d1411d5d50f309792aae1f81a4d3d140e
Jihyun-Choi <034179@naver.com>
2022-11-22 23:33:14 +0900

    22.11.22 PS

---
+++ BOJ/python/backtracking/15657.py
@@ -0,0 +1,12 @@
+def dfs(idx):
+    if len(sequence) == M:
+        print(*sequence)
+        return
+    for i in range(idx, N):
+        sequence.append(num[i])
+        dfs(i)
+        sequence.pop()
+N, M = map(int, input().split())
+num = sorted(list(map(int, input().split())))
+sequence = []
+dfs(0)
