92f476e369a62d1161bdb1daba5ae5a17856c59c
Jihyun-Choi <034179@naver.com>
2023-02-15 23:43:48 +0900

    23.2.15 PS

---
+++ BOJ/python/DP/1520.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+M, N = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(M)]
+dp = [[-1] * N for _ in range(M)]
+dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
+def dfs(x, y):
+    if x == M-1 and y == N-1:
+        return 1
+    if dp[x][y] != -1:
+        return dp[x][y]
+    cnt = 0
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < M and 0 <= ny < N and graph[x][y] > graph[nx][ny]:
+            cnt += dfs(nx, ny)
+    dp[x][y] = cnt
+    return dp[x][y]
+print(dfs(0,0))
