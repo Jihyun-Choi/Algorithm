04c60e4af3a770dd35671fcaa4e28e7bac8cb420
Jihyun-Choi <034179@naver.com>
2023-04-11 12:42:55 +0900

    23.4.11 PS

---
+++ BOJ/python/string/10769.py
@@ -0,0 +1,7 @@
+message = input()
+happy = message.count(":-)")
+sad = message.count(":-(")
+if happy > sad : print("happy")
+elif happy < sad : print("sad")
+elif happy == sad == 0 : print("none")
+elif happy == sad : print("unsure")
