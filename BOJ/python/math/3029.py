7b97557d76c963f07394e46ce920739cb4f55574
Jihyun-Choi <034179@naver.com>
2023-05-14 17:46:48 +0900

    23.5.14 PS

---
+++ BOJ/python/math/3029.py
@@ -0,0 +1,6 @@
+h1, m1, s1 = map(int, input().split(':'))
+h2, m2, s2 = map(int, input().split(':'))
+t1 = h1*60*60 + m1*60 + s1
+t2 = h2*60*60 + m2*60 + s2
+t = t2 - t1 if t2 > t1 else t2-t1+24*60*60
+print("%02d:%02d:%02d" % (t//60//60, t//60 % 60, t%60))
